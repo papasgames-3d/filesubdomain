a89a8c81cc3b3511abac5266223d0fe6c40ab148
Merchant <merchantkaren602@gmail.com>
2025-03-28 14:59:00 +0700

    Revert "remover opps"
    This reverts commit 4eb436bb1bbc9e2319deaa9e5c950ea19053fe88.

--- projects/cookie-clicker/main.js
+++ projects/cookie-clicker/main.js
@@ -1174,7 +1174,7 @@ Game.Launch=function()
 		l('javascriptError').innerHTML=
 		'<div class="title">Oops. Wrong address!</div>'+
 		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="https://game.monkeymart.one/projects/cookie-clicker/index.html" target="_blank">play Cookie Clicker over here</a>!<br>'+
+		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
 		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
 	}
 	Game.timedout=false;
